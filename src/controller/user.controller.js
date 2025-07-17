66b1a607971e60ba2741780c770e2468c25701d0
Yamraj-Dev <yamrajgaming007@gmail.com>
2025-07-17 19:16:33 +0530

    feat: add ChangePassword functionality to update user password

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -161,6 +161,35 @@ export const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 });
 
-export const GetCurrentUSer = asyncHandler( async (req, res) => {
+export const GetCurrentUSer = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, req.user, "User fetched successfully"));
-})
+});
+
+export const ChangePassword = asyncHandler(async (req, res) => {
+    const {oldPassword, newPassword} = req.body;
+
+    if(!oldPassword) {
+        throw new ApiError(400, "Enter your old password!")
+    };
+
+    const user = await User.findById(req.user?._id);
+
+    if(!user) {
+        throw new ApiError(400, "unauthorized request");
+    };
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password");
+    }
+
+    user.password = newPassword;
+
+    await user.save({ validateBeforeSave: false });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Password chnaged successfully"))
+
+});
